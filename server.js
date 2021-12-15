7c56c01d9410b849bdaa893da725197ed4baa553
Lihem <91031530+ezelnali@users.noreply.github.com>
2021-12-15 09:57:39 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,51 @@
+
+// use the express that was downloaded to the json
+const express = require("express");
+// cors is Cross-Origin Resource Sharing a mechanism that allows 
+//or restricts requested resourses on a web server depend on where 
+//the HTTP request was intiated. This policy is used to secure a 
+//certain web server from access by other webside or domain. 
+const cors = require("cors");
+
+const app = express();
+
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// sets the route to tutorial.routes
+require("./app/routes/tutorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
